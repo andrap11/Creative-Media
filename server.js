b876dcab441f26298b9c9f55e99a0dd925512def
andrap11 <andrap11@gmail.com>
2015-04-08 18:18:54 +0300

    Changed dirname

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express"),
         app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/build'));
 app.listen(parseInt(process.env.PORT) || 3010);
